ec20fd0142e8999a102fdc070ff00a2bcdc2d20a
Nirmal Kumar Arya <nirmalkumararya@gmail.com>
2017-05-29 01:19:46 +0530

    module and controller creation

---
+++ Block/Index/Index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Implement Guest Order History
+ * Copyright (C) 2017  Born
+ * 
+ * This file included in Nirmalarya/Guestorderhistory is licensed under OSL 3.0
+ * 
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Nirmalarya\Guestorderhistory\Block\Index;
+
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ Controller/Index/Index.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Implement Guest Order History
+ * Copyright (C) 2017  Born
+ * 
+ * This file included in Nirmalarya/Guestorderhistory is licensed under OSL 3.0
+ * 
+ * http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * Please see LICENSE.txt for the full text of the OSL 3.0 license
+ */
+
+namespace Nirmalarya\Guestorderhistory\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\App\Action\Context  $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute view action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
